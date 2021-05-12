558c69fe14f2e52b20706c6cddb779035ad47012
chenzhimin888 <854032732@qq.com>
2021-05-12 17:39:57 +0800

    test

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author shkstrat
+ * @creat 2021-05-12 17:37
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
