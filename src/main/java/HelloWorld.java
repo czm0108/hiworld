bdb67bb22c0d053c297a3653297d9b98e54b1b5c
chenzhimin888 <854032732@qq.com>
2021-05-12 17:49:16 +0800

    test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
         System.out.println("hello world");
         System.out.println("aaaa");
         System.out.println("bbbbb");
+        System.out.println("cccc");
     }
 }
